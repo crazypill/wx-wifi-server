2e51ba04e353160d6cbe3fa3d9e4e2970c6e7471
Alex Lelievre <alex@folabs.com>
2020-07-24 12:02:34 -0700

    Changed logging format so time works better in python parser.

--- wx-wifi-server/main.c
+++ wx-wifi-server/main.c
@@ -80,7 +80,7 @@ int read_from_client( int filedes )
     {
         time_t t = time(NULL);
         struct tm tm = *localtime(&t);
-        fprintf( s_logFile, "%d-%02d-%02d, %02d:%02d:%02d, %s\n", tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec, buffer );
+        fprintf( s_logFile, "%d-%02d-%02d %02d:%02d:%02d, %s\n", tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec, buffer );
         fflush( s_logFile ); // in case someone is watching it...
     }
 
